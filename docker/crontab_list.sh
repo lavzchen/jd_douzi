615041065bcd8fe400186763ad98de20a77c7b7d
shuye <80120075+wuzhi01@users.noreply.github.com>
2021-03-19 14:11:19 +0800

    一加盲盒

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -27,6 +27,9 @@
 1 0,8 * * * node /scripts/jd_mgold.js >> /scripts/logs/jd_mgold.log 2>&1
 #5g盲盒
 1 9,12,19 * * * node /scripts/jd_super5g.js >> /scripts/logs/jd_super5g.log 2>&1
+#jd_oneplus盲盒
+1 3,13 * * * node /scripts/jd_oneplus.js >> /scripts/logs/jd_oneplus.log 2>&1
+
 ##############长期活动##############
 # 签到
 0 0,18 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
